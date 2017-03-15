e6d99979f0466b1df68329e5118a1647f1a5511e
anutony <aisavaria@gmail.com>
2017-03-15 16:40:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,16 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-
-//move the img
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-marginLeft = marginLeft + 1;
-img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50)
+button.onclick = function () {
+    //make a request to counter endpoint 
+    
+    //capture the response and store it in a variable 
+    
+    //render the variable in the correct span
+    
+var span = document.getElementById('count');  
+span.innerHTML = counter.toString();
 };
+
+
