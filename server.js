f07c4b25a41a6654694277a0a5be11e110500e78
anutony <aisavaria@gmail.com>
2017-03-17 15:57:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,12 +124,14 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-app.get('/submit-name',function(req, res) {
-var name = req.params.name;
-res.send(JSON.stringify(names));
+
+
+//var names = [];
+////app.get('/submit-name/:name', function(req, res) {
+////var name = req.params.name;
+//res.send(JSON.stringify(names));
     
-names.push(name);
+//names.push(name);
 });
 
 
