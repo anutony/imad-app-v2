1806b2eb35293049b5e8735f98d899223b4a6847
anutony <aisavaria@gmail.com>
2017-03-17 15:31:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,6 +36,6 @@ submit.onclick = function () {
     for (var i=0;i<names.length;i++) {
         list += '<li>' + names[i] + '</li>'
     }
-    var ul = document.getElementById('list');
+    var ul = document.getElementById('namelist');
     ul.innerHTML = list;
 };
