61c013bf877df815561e76eacad1d342fc3868b7
anutony <aisavaria@gmail.com>
2017-03-21 06:35:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,11 +110,11 @@ res.send(counter.toString());
 });
 
 var names = [];
-app.get('/submit-name', function(req, res) {
+app.get('/submit-name/:name', function(req, res) {
     //get the name from the request 
-var name = req.query.name;
+//var name = req.query.name;
 
-    
+var name = req.params.name;    
 names.push(name);
 res.send(JSON.stringify(names));
 });
