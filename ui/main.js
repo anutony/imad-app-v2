9c9d7bc31eb785cae0cc83c721ec670be5d83a94
anutony <aisavaria@gmail.com>
2017-03-21 07:04:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,13 +33,26 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //make a request to server and send the nemes
     
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status == 200) {
+    
+    
     //capture the list and render it as names
-    var names = ['name1', 'name2', 'name3', 'name4'];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for (var i=0; i<names.length; i++) {
         list += '<li>' + names[i] + '</li>'
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+}
+}
+//not done yet
 };
-
+ //make the req
+    request.open('GET', 'http://anutony.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
